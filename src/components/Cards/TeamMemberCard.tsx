bb33af1d13e6e7eb11e83ba7ded9f6a35fbb5636
Dave Gomez <davegomez426@gmail.com>
2025-10-24 11:44:46 -0300

    Correccion de responsividad del card de miembros

--- src/components/Cards/TeamMemberCard.tsx
+++ src/components/Cards/TeamMemberCard.tsx
@@ -33,23 +33,31 @@ const TeamMemberCard = ({
 }: TeamMemberCardProps) => {
   return (
     <div
-      className={`relative w-[377px] h-[723px] rounded-[16px] flex flex-col justify-start border-[1px] border-[#22AF52] p-[32px] gap-4 m-auto mb-10`}
+      className={`relative w-[80vw] h-[65vh] md:w-[377px] md:h-[650px] lg:w-[377px] lg:h-[653px] rounded-[16px] flex flex-col border border-[#22AF52] p-6 gap-4 m-auto lg:mb-10 items-center text-center bg-white`}
     >
       <img
         src={imageUrl}
         alt={name}
-        className="block  w-[313px] h-[280px] object-cover transition-transform duration-300 hover:scale-105 rounded-[12px]"
+        className="w-[50vw] h-[50vw] max-w-[313px] max-h-[280px] object-cover rounded-[12px] transition-transform duration-300 hover:scale-105 mx-auto"
       />
-      <h3 className={`${sora.className} text-[24px]  text-[#07090D]`}>
+      <h3
+        className={`${sora.className} text-[20px] sm:text-[22px] lg:text-[24px] text-[#07090D] mt-4`}
+      >
         {name}
       </h3>
-      <line className="border-b border-[#22AF52] mb-4" />
-      <h5 className={`${inter.className} text-[16px] text-[#22AF52] mb-[28px]`}>
-        {position}
-      </h5>
-      <p className={`${rubik.className} text-[16px] text-[#586174]`}>
-        {description}
-      </p>
+      <div className="border-b border-[#22AF52] w-1/2 mb-2" />
+      <div>
+        <h5
+          className={`${inter.className} text-[14px] sm:text-[15px] lg:text-[16px] text-[#22AF52] mb-4 text-start justify-self-start`}
+        >
+          {position}
+        </h5>
+        <p
+          className={`${rubik.className} text-[14px] sm:text-[15px] text-[#586174] leading-relaxed text-start`}
+        >
+          {description}
+        </p>
+      </div>
     </div>
   );
 };
