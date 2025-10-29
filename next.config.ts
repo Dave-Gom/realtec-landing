ec8f7bf20ae4691e1ae43c8b071552370d2b6618
David Gómez <davegomez426@gmail.com>
2025-10-28 21:02:11 -0300

    Configuraciones de export listos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // genera /out
+  images: { unoptimized: true }, // evita optimizador (requiere Node)
+  trailingSlash: true, // mejor para hosting simple (carpetas/)
+  reactStrictMode: true,
 };
 
 export default nextConfig;
